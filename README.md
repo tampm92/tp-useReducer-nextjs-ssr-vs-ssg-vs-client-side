20f935a97aebd292d53f87d1cb52a118732444dc
tampm92 <phanminhtam1992@gmail.com>
2021-04-17 20:01:46 +0200

    feature: update dumbData

--- README.md
+++ README.md
@@ -111,7 +111,111 @@ You can read this [blog](https://blog.tampm.com/posts/next-js-server-side-render
 This example help you setup 2 Store
 
 - 1 for global (see `/store/GlobalStore.js`)
+
+```jsx
+import { useReducer } from 'react'
+
+const initialState = {
+  isLoading: false
+}
+
+export const actionTypes = {
+  SET_LOADING: 'SET_LOADING'
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.SET_LOADING:
+      return { ...state, isLoading: action.payload }
+    default:
+      return state
+  }
+}
+
+export const useGlobalStore = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return {
+    state,
+    dispatch
+  }
+}
+
+// actions
+const setLoading = (data) => ({
+  type: actionTypes.SET_LOADING,
+  payload: data
+})
+
+export const GlobalActions = {
+  setLoading
+}
+```
+
 - 1 for special page (see `/store/ProductStore.js`)
+
+```jsx
+import { useReducer } from 'react'
+
+const initialState = {
+  list: [],
+  cart: [],
+  detail: {}
+}
+
+export const actionTypes = {
+  SET_PRODUCT_LIST: 'SET_PRODUCT_LIST',
+  SET_PRODUCT_DETAIL: 'SET_PRODUCT_DETAIL',
+  SET_PRODUCT_CART: 'SET_PRODUCT_CART'
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case actionTypes.SET_PRODUCT_LIST: {
+      return { ...state, list: action.payload }
+    }
+    case actionTypes.SET_PRODUCT_DETAIL: {
+      return { ...state, detail: action.payload }
+    }
+    case actionTypes.SET_PRODUCT_CART: {
+      return { ...state, cart: action.payload }
+    }
+    default:
+      return state
+  }
+}
+
+export const useProductStore = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return {
+    state,
+    dispatch
+  }
+}
+
+// actions
+const setProductList = (data) => ({
+  type: actionTypes.SET_PRODUCT_LIST,
+  payload: data
+})
+
+const setProductDetail = (data) => ({
+  type: actionTypes.SET_PRODUCT_DETAIL,
+  payload: data
+})
+
+const setProductCart = (data) => ({
+  type: actionTypes.SET_PRODUCT_CART,
+  payload: data
+})
+
+export const ProductActions = {
+  setProductList,
+  setProductDetail,
+  setProductCart
+}
+```
 
 ### **How use**
 
--- shared/dumbData.js
+++ shared/dumbData.js
@@ -1,15 +1,15 @@
 const dumbData = {
   products: [
-    { id: '5eb949a926195f7545193d31', slug: 'bo-bun', name: 'Bo bun', price: 8.5, description: 'Nems au porc, bœuf, salade, vermicelles de riz, cacahuète, sauce de poisson', image: 'https://poitiers.tamy.biz/uploads/5eb949a926195f7545193d31/W_Bo_Bun.jpg' },
-    { id: '5eb949a926195f7545193d32', slug: 'bo-bun-vegetariens', name: 'Bo bun végétariens', price: 8.5, description: 'Tofu, salade, vermicelles de riz, cacahuète, sauce de soja', image: 'https://poitiers.tamy.biz/uploads/5eb949d426195f7545193d32/Bo_Bun_V%C3%A9g%C3%A9tarien.jpg' },
-    { id: '5eb9491126195f7545193d30', slug: 'salades-de-bambou', name: 'Salades de bambou', price: 2.9, description: 'Pousse de bambou, poulet, carrotte, radis blanc, concombre, œuf, cacahuète, sauce de poisson', image: 'https://poitiers.tamy.biz/uploads/5eb9491126195f7545193d30/Salade_Bambou_V%C3%A9g%C3%A9tarienne.jpg' },
-    { id: '5eb948c126195f7545193d2e', slug: 'riz-cantonnais', name: 'Riz cantonnais', price: 2.9, description: 'Riz, œuf, petits pois, porc laqué, carotte', image: 'https://poitiers.tamy.biz/uploads/5eb948c126195f7545193d2e/riz_cantonnais.jpg' },
-    { id: '5eb9485626195f7545193d2d', slug: 'rouleaux-de-printemps', name: 'Rouleaux de Printemps', price: 2.9, description: 'Galette de riz, salade, porc, crevette, vermicelles de riz, œuf, échalotte frite', image: 'https://poitiers.tamy.biz/uploads/5eb9485626195f7545193d2d/Rouleaux_de_printemps.jpg' },
-    { id: '5eb87c9926195f7545193d2c', slug: 'banh-bao', name: 'Bánh bao', price: 2.9, description: 'Brioche farcie, cuite à la vapeur.', image: 'https://poitiers.tamy.biz/uploads/5eb87c9926195f7545193d2c/Banh_bao.jpg' },
-    { id: '5eb87ba126195f7545193d2b', slug: 'nems-au-porc', name: 'Nems au porc', price: 2.9, description: 'Une portion de deux nems avec salade, menthe et sauce.', image: 'https://poitiers.tamy.biz/uploads/5eb87ba126195f7545193d2b/Nems_au_porc__.jpg' },
-    { id: '5eb94c4026195f7545193d3c', slug: 'nouilles-vegetarien', name: 'Nouilles Végétarien', price: 8.5, description: 'Nouilles sautées au tofu et avec des légumes', image: 'https://poitiers.tamy.biz/uploads/5eb94c4026195f7545193d3c/WNvege.jpg' },
-    { id: '5eb94b3826195f7545193d37', slug: 'riz-porc-laque', name: 'Riz Porc laqué ', price: 8.5, description: 'Porc laqué servi avec riz et concombre', image: 'https://poitiers.tamy.biz/uploads/5eb94b3826195f7545193d37/Wporclaque.jpg' },
-    { id: '5eb94a4526195f7545193d34', slug: 'bo-bun-au-poulet', name: 'Bo bun au poulet', price: 8.5, description: 'Nems au poulet, poulet sauté, salades, vermicelle de riz, cacahuète, sauce de poisson', image: 'https://poitiers.tamy.biz/uploads/5eb94a4526195f7545193d34/Wbobunpoulet.jpg' },
+    { id: '5eb949a926195f7545193d31', slug: 'bo-bun', name: 'Bo bun', price: 8.5, description: 'Nems au porc, bœuf, salade, vermicelles de riz, cacahuète, sauce de poisson', image: 'https://picsum.photos/200' },
+    { id: '5eb949a926195f7545193d32', slug: 'bo-bun-vegetariens', name: 'Bo bun végétariens', price: 8.5, description: 'Tofu, salade, vermicelles de riz, cacahuète, sauce de soja', image: 'https://picsum.photos/200' },
+    { id: '5eb9491126195f7545193d30', slug: 'salades-de-bambou', name: 'Salades de bambou', price: 2.9, description: 'Pousse de bambou, poulet, carrotte, radis blanc, concombre, œuf, cacahuète, sauce de poisson', image: 'https://picsum.photos/200' },
+    { id: '5eb948c126195f7545193d2e', slug: 'riz-cantonnais', name: 'Riz cantonnais', price: 2.9, description: 'Riz, œuf, petits pois, porc laqué, carotte', image: 'https://picsum.photos/200' },
+    { id: '5eb9485626195f7545193d2d', slug: 'rouleaux-de-printemps', name: 'Rouleaux de Printemps', price: 2.9, description: 'Galette de riz, salade, porc, crevette, vermicelles de riz, œuf, échalotte frite', image: 'https://picsum.photos/200' },
+    { id: '5eb87c9926195f7545193d2c', slug: 'banh-bao', name: 'Bánh bao', price: 2.9, description: 'Brioche farcie, cuite à la vapeur.', image: 'https://picsum.photos/200' },
+    { id: '5eb87ba126195f7545193d2b', slug: 'nems-au-porc', name: 'Nems au porc', price: 2.9, description: 'Une portion de deux nems avec salade, menthe et sauce.', image: 'https://picsum.photos/200' },
+    { id: '5eb94c4026195f7545193d3c', slug: 'nouilles-vegetarien', name: 'Nouilles Végétarien', price: 8.5, description: 'Nouilles sautées au tofu et avec des légumes', image: 'https://picsum.photos/200' },
+    { id: '5eb94b3826195f7545193d37', slug: 'riz-porc-laque', name: 'Riz Porc laqué ', price: 8.5, description: 'Porc laqué servi avec riz et concombre', image: 'https://picsum.photos/200' },
+    { id: '5eb94a4526195f7545193d34', slug: 'bo-bun-au-poulet', name: 'Bo bun au poulet', price: 8.5, description: 'Nems au poulet, poulet sauté, salades, vermicelle de riz, cacahuète, sauce de poisson', image: 'https://picsum.photos/200' },
   ]
 }
 
